19f78b3014af4c67f3080a27a35f67bc1e6d32f4
Edgar Morales <edgar.morales@softtek.com>
2020-02-07 03:54:52 -0600

    Consuming  YoutubePlayerHistory
    Consuming service published on IIS Web Server

--- RockolaApp/Controllers/HomeController.cs
+++ RockolaApp/Controllers/HomeController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 using Google.Apis.Services;
 using Google.Apis.YouTube.v3;
 using Google.Apis.YouTube.v3.Data;
 using RockolaApp.ServiceReferenceWCFYoutube;
+using RockolaApp.Models;
+using SearchResultCustomized = RockolaApp.Models.SearchResultCustomized;
 
 
 namespace RockolaApp.Controllers
@@ -18,16 +21,61 @@ namespace RockolaApp.Controllers
             return View("");
         }
 
+
+        [HttpPost]
+        //public  SendVideoTOWebAPI(SearchResultCustomized video)
+        public ActionResult SendVideoTOWebAPI(SearchResultCustomized video)
+        {
+            IEnumerable<Models.SearchResultCustomized> searchResultCustomizeds = null;
+
+            using (var client = new HttpClient())
+            {
+                //client.BaseAddress = new Uri("http://localhost:9810/api/");
+                client.BaseAddress = new Uri("http://localhost:80/youtplayerhistory/api/videos");
+                var responseTask = client.PostAsJsonAsync<Models.SearchResultCustomized>("videos", video);
+                
+                //new StringContent("{ \"firstName\": \"John\" }", System.Text.Encoding.UTF8, "application/json"))
+
+                //HTTP GET/WebApplicationYT
+                //var responseTask = client.GetAsync($"youtube/searchyoutubevideo?keyword={keyword}");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+
+                    var readTask = result.Content.ReadAsAsync<IList<Models.SearchResultCustomized>>();
+                    readTask.Wait();
+
+                    searchResultCustomizeds = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    searchResultCustomizeds = Enumerable.Empty<Models.SearchResultCustomized>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return Json(searchResultCustomizeds, JsonRequestBehavior.AllowGet);//"chamara", JsonRequestBehavior.AllowGet);
+            
+        }
+
         [HttpGet]
         public ActionResult YoutubeVideosSearcher(string keyword)
         {
-
+            //1st Method
             // var searchResults = SearchVideo(keyword);
-            var searchResults = SearchVideoFromWCFServiceApp(keyword);
+
+            //2nd WCF
+            //var searchResults = SearchVideoFromWCFServiceApp(keyword);
+
+            //Web API
+            var searchResults = SearchVideoFromWebAPI(keyword); 
             return PartialView("Searcher",searchResults);
         }
-
-
         public IList<SearchResult> SearchVideo(string keyword)
         {
             //Construyendo el servicio de Youtube
@@ -45,16 +93,46 @@ namespace RockolaApp.Controllers
             IList<SearchResult> searchResults = searchResponse.Items;
             return searchResults;
         }
+        public IList<Models.SearchResultCustomized> SearchVideoFromWebAPI(string keyword)
+        {
+            IEnumerable<Models.SearchResultCustomized> searchResultCustomizeds = null;
+
+            using (var client = new HttpClient())
+            {
+                //client.BaseAddress = new Uri("http://localhost:9810/api/");
+                client.BaseAddress = new Uri("http://localhost:80/WebApplicationYT/api/");
+
+                //HTTP GET/WebApplicationYT
+                var responseTask = client.GetAsync($"youtube/searchyoutubevideo?keyword={keyword}");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
 
 
-        
+                    var readTask = result.Content.ReadAsAsync<IList<Models.SearchResultCustomized>>();
+                    readTask.Wait();
+
+                    searchResultCustomizeds = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    searchResultCustomizeds = Enumerable.Empty<Models.SearchResultCustomized>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return searchResultCustomizeds.ToList();
+        }
         public IList<ServiceReferenceWCFYoutube.SearchResultCustomized>SearchVideoFromWCFServiceApp(string keyword)
         {
             ServiceReferenceWCFYoutube.Service1Client service= new Service1Client();
             var listVideos = service.SearchYoutubeVideo(keyword);
             return listVideos;
         }
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
---
+++ RockolaApp/Models/SearchResultCustomized.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RockolaApp.Models
+{
+    public class SearchResultCustomized
+    {
+        private int Id { get; set; }
+        string videoId;
+        string imageURL;
+        string title;
+
+        public string VideoId
+        {
+            get { return videoId; }
+            set { videoId = value; }
+        }
+
+        public string ImageURL
+        {
+            get { return imageURL; }
+            set { imageURL = value; }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+    }
+}
